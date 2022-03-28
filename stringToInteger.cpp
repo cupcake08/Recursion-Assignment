ade80ae9375222396ff598bc12fc02375ab6965c
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2022-03-28 19:13:45 +0530

    Update stringToInteger.cpp

--- stringToInteger.cpp
+++ stringToInteger.cpp
@@ -1,18 +1,20 @@
-//for complete problem refer to coding ninjas platform
-
-#include <cstring>
-int stringToInteger(char *input,int last)
-{
-    if(last==0)
-    {
-        return input[last]-48;
-    }
-    int smallAns=stringToInteger(input,last-1);
-    int a = input[last]-48;
-    return smallAns*10+a;
+//recursive
+int res = 0;
+int stringToNumber(char *input,int si=0) {
+    if(input[si] == '\0') return res;
+    res = res * 10 + (input[si] - '0');
+    int temp = stringToNumber(input,si+1);
+    return res;
 }
-int stringToInteger(char *str)
-{
-    int last=strlen(str)-1;
-    return stringToInteger(str,last);
+
+//iterative
+int stringToNumber(char *in){
+    bool flag = 1;
+    int res = 0;
+    for(int i=0;in[i] != '\0';i++){
+        if(in[i]==0 && flag) continue;
+        flag = 0;
+        res = res * 10 + (in[i] - '0');
+    }
+    return res;
 }
