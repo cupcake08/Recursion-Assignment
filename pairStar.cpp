19d7e48d590d4f40f7e78be35503da26fc97be6b
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2022-03-28 19:28:03 +0530

    Update pairStar.cpp

--- pairStar.cpp
+++ pairStar.cpp
@@ -1,26 +1,33 @@
-//for complete problem refer to coding ninjas platform
+// #include <cstring>
+// void pairStar(char input[],int si=0) {
+//     // Write your code here
+// 	if(input[si]=='\0') return;
+//     pairStar(input,si+1);
+//     if(input[si]==input[si+1]){
+//         int len = strlen(input);
+//         for(int i=len;i>=si+1;i--){
+//             input[i+1]=input[i];
+//         }
+//         input[si+1]='*';
+//     }
+// }
 
-#include <cstring>
-void pairStar(char str[],int start=0)
-{
-  //base case
-  if(str[start]=='\0')
-  {
-    return;
-  }
-  pairStar(str,start+1);
-  if(str[start]==str[start+1])
-  {
-    int l=strlen(str);
-    //extending STRING
-    str[l+1]='\0';
-    int i=l-1;
-    for(;i>=start+1;i--)
-    {
-      str[i+1]=str[i];
+//-> Optimized Version
+// going from start, therefore no need to include "cstring" library 
+// so no need to calculate length everytime
+void pairStar(char *in,int si=0){
+    if(in[si]=='\0') return; ///base case
+    pairStar(in,si+1);
+    if(in[si] == in[si+1]){
+        int i = si+1;
+        char x = in[i];
+        in[i++] = '*';
+        for(;in[i] != '\0';i++){
+            char y = in[i];
+            in[i] = x;
+            x = y;
+        }
+        in[i] = x;
+        in[i+1] = '\0';
     }
-    //entering * between
-    str[start+1]='*';
-
-  }
 }
